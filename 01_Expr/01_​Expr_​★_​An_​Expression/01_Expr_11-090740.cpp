989b4f6a95447d2997238d59af6d25473a0fafa8
tahto <phalat.tah@gmail.com>
2024-08-28 05:39:12 +0700

    First Committing

---
+++ 01_Expr/01_​Expr_​★_​An_​Expression/01_Expr_11-090740.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int fac=1 ;
+    for(int i=1 ; i<=10 ; i++) fac *= i;
+
+    cout << (M_PI-fac/pow(8,8)+pow(log(9.7),(7/sqrt(71)-sin(40*M_PI/180)))) /pow(1.2,cbrt(2.3));
+}
